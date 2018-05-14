c46801e3e4dffdcfa9c4913c808a3e6199e1e733
Tom Pearson <tom.g.pearson@github.com>
2018-05-14 09:26:07 +0100

    skip const declaration

--- data/generate-comment-forms.js
+++ data/generate-comment-forms.js
@@ -5,15 +5,14 @@ const speakingurl = require('speakingurl');
 const data = fs.readFileSync('books.csv','utf-8');
 dsv.csvParse(data)
     .forEach((book, i)=>{
-        const frontMatter=`---
-title: ${book.title}
-author: ${book.authors}
-reading date: ${book.date}
----`;
         try{
             fs.writeFileSync(
                 `./comments/${i}-${speakingurl(book.title)}-${speakingurl(book.authors)}.yml`,
-                frontMatter,
+                `---
+title: ${book.title}
+author: ${book.authors}
+reading date: ${book.date}
+---`,
                 {flag:'wx'}
             )
         }catch(err){
